6dff24afdd2b9c3bbba6bef40e18d1fc8ce3da77
Jason Walton <jwalton@benbria.ca>
2018-05-18 07:27:37 -0400

    test: Add more test cases.

--- test/integration.test.js
+++ test/integration.test.js
@@ -20,42 +20,6 @@ test('Parse with "conventional-changelog-angular" by default', async t => {
   t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 2, 'minor'));
 });
 
-test('Correctly handle squash merges', async t => {
-  const message =
-    'Squashed commit of the following:\n' +
-    '\n' +
-    'commit 06cfb67026eecec2ad54fd39fc50cba9080d383c\n' +
-    'Author: Jason Walton <fakeaddress@fake.com>\n' +
-    'Date:   Thu May 17 11:21:50 2018 -0400\n' +
-    '\n' +
-    '    fix: More fixes\n' +
-    '\n' +
-    '    Blah blah blah.\n' +
-    '\n' +
-    '    Fixes #20\n' +
-    '\n' +
-    'commit b894885ae467975226e81080a35625d247796960\n' +
-    'Author: Jason Walton <fakeaddress@fake.com>\n' +
-    'Date:   Thu May 17 11:13:25 2018 -0400\n' +
-    '\n' +
-    '    feat: Add moar features!\n';
-
-  const splitCommits = [
-    'fix: More fixes\n\nBlah blah blah.\n\nFixes #20',
-    'feat: Add moar features!'
-  ];
-
-  const commits = [{message}];
-  const releaseType = await commitAnalyzer({}, {commits, logger: t.context.logger});
-
-  t.is(releaseType, 'minor');
-  t.true(t.context.log.calledWith('Analyzing commit: %s', splitCommits[0]));
-  t.true(t.context.log.calledWith('The release type for the commit is %s', 'patch'));
-  t.true(t.context.log.calledWith('Analyzing commit: %s', splitCommits[1]));
-  t.true(t.context.log.calledWith('The release type for the commit is %s', 'minor'));
-  t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 2, 'minor'));
-});
-
 test('Accept "preset" option', async t => {
   const commits = [{message: 'Fix: First fix (fixes #123)'}, {message: 'Update: Second feature (fixes #456)'}];
   const releaseType = await commitAnalyzer({preset: 'eslint'}, {commits, logger: t.context.logger});
@@ -276,4 +240,123 @@ test('Throw error if "releaseRules" reference invalid commit type', async t => {
 test('Re-Throw error from "conventional-changelog-parser"', async t => {
   const commits = [{message: 'Fix: First fix (fixes #123)'}, {message: 'Update: Second feature (fixes #456)'}];
   await t.throws(commitAnalyzer({parserOpts: {headerPattern: '\\'}}, {commits, logger: t.context.logger}));
+});
+
+test('Correctly handle squash merges', async t => {
+  const message =
+    'Squashed commit of the following:\n' +
+    '\n' +
+    'commit 06cfb67026eecec2ad54fd39fc50cba9080d383c\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:21:50 2018 -0400\n' +
+    '\n' +
+    '    fix: More fixes\n' +
+    '\n' +
+    '    Blah blah blah.\n' +
+    '\n' +
+    '    Fixes #20\n' +
+    '\n' +
+    'commit b894885ae467975226e81080a35625d247796960\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:13:25 2018 -0400\n' +
+    '\n' +
+    '    feat: Add moar features!\n';
+
+  const splitCommits = ['fix: More fixes\n\nBlah blah blah.\n\nFixes #20', 'feat: Add moar features!'];
+
+  const commits = [{message}];
+  const releaseType = await commitAnalyzer({}, {commits, logger: t.context.logger});
+
+  t.is(releaseType, 'minor');
+  t.true(t.context.log.calledWith('Analyzing commit: %s', splitCommits[0]));
+  t.true(t.context.log.calledWith('The release type for the commit is %s', 'patch'));
+  t.true(t.context.log.calledWith('Analyzing commit: %s', splitCommits[1]));
+  t.true(t.context.log.calledWith('The release type for the commit is %s', 'minor'));
+  t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 2, 'minor'));
+});
+
+test('Treat modified squash merges like regular commmit messages', async t => {
+  const message =
+    'fix: This is only a fix.\n' +
+    '\n' +
+    'commit 06cfb67026eecec2ad54fd39fc50cba9080d383c\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:21:50 2018 -0400\n' +
+    '\n' +
+    '    fix: More fixes\n' +
+    '\n' +
+    '    Blah blah blah.\n' +
+    '\n' +
+    '    Fixes #20\n' +
+    '\n' +
+    'commit b894885ae467975226e81080a35625d247796960\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:13:25 2018 -0400\n' +
+    '\n' +
+    '    feat: Add moar features!\n';
+
+  const commits = [{message}];
+  const releaseType = await commitAnalyzer({}, {commits, logger: t.context.logger});
+
+  t.is(releaseType, 'patch');
+  t.true(t.context.log.calledWith('Analyzing commit: %s', commits[0].message));
+  t.true(t.context.log.calledWith('The release type for the commit is %s', 'patch'));
+  t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 1, 'patch'));
+});
+
+test('Treat modified squash merges with a BREAKING CHANGE as breaking', async t => {
+  const message =
+    'fix: This is only a fix.\n' +
+    '\n' +
+    'commit 06cfb67026eecec2ad54fd39fc50cba9080d383c\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:21:50 2018 -0400\n' +
+    '\n' +
+    '    fix: More fixes\n' +
+    '\n' +
+    '    Blah blah blah.\n' +
+    '\n' +
+    '    BREAKING CHANGE: Boom\n' +
+    '\n' +
+    'commit b894885ae467975226e81080a35625d247796960\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:13:25 2018 -0400\n' +
+    '\n' +
+    '    feat: Add moar features!\n';
+
+  const commits = [{message}];
+  const releaseType = await commitAnalyzer({}, {commits, logger: t.context.logger});
+
+  t.is(releaseType, 'major');
+  t.true(t.context.log.calledWith('Analyzing commit: %s', commits[0].message));
+  t.true(t.context.log.calledWith('The release type for the commit is %s', 'major'));
+  t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 1, 'major'));
+});
+
+test('Handle broken squash merge like a regular commit', async t => {
+  // This looks like a squash merge, but it's invalid because it doesn't
+  // follow the "commit, author, date, indentend body" pattern.
+  const message =
+    'Squashed commit of the following:\n' +
+    '\n' +
+    'commit 06cfb67026eecec2ad54fd39fc50cba9080d383c\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n' +
+    'Date:   Thu May 17 11:21:50 2018 -0400\n' +
+    '\n' +
+    '    fix: More fixes\n' +
+    '\n' +
+    '    Blah blah blah.\n' +
+    '\n' +
+    '    Fixes #20\n' +
+    '\n' +
+    'commit b894885ae467975226e81080a35625d247796960\n' +
+    'Author: Jason Walton <fakeaddress@fake.com>\n';
+
+  const commits = [{message}];
+  const releaseType = await commitAnalyzer({}, {commits, logger: t.context.logger});
+
+  t.is(releaseType, null);
+  t.true(t.context.log.calledWith('Analyzing commit: %s', commits[0].message));
+  t.true(t.context.log.calledWith('The commit should not trigger a release'));
+  t.true(t.context.log.calledWith('Analysis of %s commits complete: %s release', 1, 'no'));
 });
