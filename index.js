0254d7a5c59ccc4692b86218554f8850ebf46682
Matt Travi <programmer@travi.org>
2024-05-24 22:33:30 -0500

    feat: support latest conventional-changelog packages
    BREAKING CHANGE: by supporting the latest major versions of conventional-changelog packages, we are
    dropping support for previous major versions of those packages due to the breaking changes between
    majors. this only impacts your project if you are installing alongside semantic-release, so updating
    those packages to latest version should be the only change you need for this update. no action
    should be necessary if you are using default semantic-release config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { isUndefined } from "lodash-es";
-import { sync as parser } from "conventional-commits-parser";
-import filter from "conventional-commits-filter";
+import { CommitParser } from "conventional-commits-parser";
+import { filterRevertedCommitsSync } from "conventional-commits-filter";
 import debugFactory from "debug";
 import loadParserConfig from "./lib/load-parser-config.js";
 import loadReleaseRules from "./lib/load-release-rules.js";
@@ -31,7 +31,8 @@ export async function analyzeCommits(pluginConfig, context) {
   const config = await loadParserConfig(pluginConfig, context);
   let releaseType = null;
 
-  filter(
+  const parser = new CommitParser(config)
+  const filteredCommits = filterRevertedCommitsSync(
     commits
       .filter(({ message, hash }) => {
         if (!message.trim()) {
@@ -41,9 +42,15 @@ export async function analyzeCommits(pluginConfig, context) {
 
         return true;
       })
-      .map(({ message, ...commitProps }) => ({ rawMsg: message, message, ...commitProps, ...parser(message, config) }))
-  ).every(({ rawMsg, ...commit }) => {
-    logger.log(`Analyzing commit: %s`, rawMsg);
+      .map((rawCommit) => ({
+        ...rawCommit,
+        ...parser.parse(rawCommit.message)
+      }))
+  );
+
+  for (const { message, ...commit } of filteredCommits) {
+    console.log(`Analyzing commit: %s`, message)
+    logger.log(`Analyzing commit: %s`, message);
     let commitReleaseType;
 
     // Determine release type based on custom releaseRules
@@ -71,11 +78,10 @@ export async function analyzeCommits(pluginConfig, context) {
 
     // Break loop if releaseType is the highest
     if (releaseType === RELEASE_TYPES[0]) {
-      return false;
+      break;
     }
+  }
 
-    return true;
-  });
   logger.log("Analysis of %s commits complete: %s release", commits.length, releaseType || "no");
 
   return releaseType;
